0aa50d9d7c00dd4fba5f4014c1b147a820e8580a
Brett Weisberg <weisberg.brett@gmail.com>
2018-09-20 00:40:53 +0000

    automatically backed up by learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -5,11 +5,21 @@ class Post < ActiveRecord::Base
   validates :category, inclusion: { in: %w( Fiction Non-Fiction)}
   validates :summary, { :length => { :maximum => 250 } }
   validate :message_validator #custom
+<<<<<<< HEAD
 
   @@words = [/Won't Believe/, /Secret/, /Top [0-9]*/, /Guess/]
  def message_validator
     if @@words.none?{|word| word.match title} #match if true returns the value  and if false = nil
         errors.add(:title, "Must change title")
     end
+=======
+
+  @@words = [/Won't Believe/, /Secret/, /Top [0-9]*/, /Guess/]
+ def message_validator
+
+   if @@words.none? {self.title  }
+     binding.pry
+  end
+>>>>>>> 2b6436ea1126a639ca45c2fab2658aea75da5bfd
  end
 end
